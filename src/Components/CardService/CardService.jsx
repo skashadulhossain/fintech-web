ada33bc0777ebfa1f60446b3583b15fc2bd1d362
junayed <junayedmollick536@gmail.com>
2024-12-11 00:41:08 +0530

    added new section

--- src/Components/CardService/CardService.jsx
+++ src/Components/CardService/CardService.jsx
@@ -6,8 +6,79 @@ const CardService = () => {
     <>
       
     <section className="CardService-section">
-        <div className="CardService-container">
-            
+        <div className="CardService-main-container">
+            <div className="CardService-title-container">
+              <h1>We Provide Digital Services</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eligendi 
+                soluta vitae ut velit officia odio nemo quaerat tempora aut.</p>
+              <button>Explore All Service</button>
+            </div>
+            <div className="CardService-card-container">
+              <div className="CardService-container-item-1">
+                <div className="CardService-container">
+                  <div className="CardService-img-box">
+                    <img src="" alt="" />
+                  </div>
+                  <div className="CardService-title-box">
+                    <h2>Travel Services</h2>
+                    <p>"Travel is the only thing you purchase that shapes you wealthy". We 
+                      thoroughly vow by this and contemplate in satisfying travel dreams 
+                      that shape you unfailingly wealthy by degrees. Pn Fintech Software 
+                      Pvt. Ltd is selling stupendous episodes over years</p>
+                  </div>
+                </div>
+              </div>
+              <div className="CardService-container-item-2">
+                <div className="CardService-container">
+                  <div className="CardService-img-box">
+                    <img src="" alt="" />
+                  </div>
+                  <div className="CardService-title-box">
+                    <h2>IT Services</h2>
+                    <p>Substantially, the prosperity of organizations in the sector hangs on their 
+                      capability to establish their services and develop their technical competence. 
+                      We provide the preeminent IT service to make you grow digitally.</p>
+                  </div>
+                </div>
+                <div className="CardService-container">
+                  <div className="CardService-img-box">
+                    <img src="" alt="" />
+                  </div>
+                  <div className="CardService-title-box">
+                    <h2>VAS Services</h2>
+                    <p>VAS is a prevalent telecommunications sector term for non-core services, 
+                      or, in essence, all services afar standard voice calls and fax 
+                      transmissions. Here, at Pn Fintech Software Pvt. Ltd we offer 
+                      world-class customer support to answer all your queries</p>
+                  </div>
+                </div>
+              </div>
+              <div className="CardService-container-item-3">
+                <div className="CardService-container">
+                  <div className="CardService-img-box">
+                    <img src="" alt="" />
+                  </div>
+                  <div className="CardService-title-box">
+                    <h2>FinTech</h2>
+                    <p>Fintech is a budgetary transformation that illustrates a growing folding 
+                      of money superintendence area. Pn Fintech Software Pvt. Ltd specializes 
+                      in technology development to aid the banking and financial industries.</p>
+                  </div>
+                </div>
+                <div className="CardService-container">
+                  <div className="CardService-img-box">
+                    <img src="" alt="" />
+                  </div>
+                  <div className="CardService-title-box">
+                    <h2>BBPS Bill Payment</h2>
+                    <p>VAS is a prevalent telecommunications sector term for non-core services, 
+                      or, in essence, all services afar standard voice calls and fax 
+                      transmissions. Here, at Pn Fintech Software Pvt. Ltd we offer 
+                      world-class customer support to answer all your queries</p>
+                  </div>
+                </div>
+              </div>
+            </div>
         </div>
     </section>
 
