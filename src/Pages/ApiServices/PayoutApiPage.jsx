a63c690cd61c34b27dcd184e60f0c40f1b35b742
Apurba Naskar <apurbanaskar3206@gmail.com>
2024-12-17 16:01:26 +0530

    Payout Page

--- src/Pages/ApiServices/PayoutApiPage.jsx
+++ src/Pages/ApiServices/PayoutApiPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MainLayout from "../../Components/Layout/MainLayout";
 import { apiServices } from "../../js/pageOptions";
 import PageRoutes from "../../Components/PageRoutes";
-import { Check, ChevronRight } from "lucide-react";
+import { Check, ChevronRight, Earth } from "lucide-react";
 
 const way = [
   { logo: "/images/login.png", title: "Login" },
@@ -119,103 +119,84 @@ const PayoutApiPage = () => {
               ))}
             </div>
 
-            <section class="service-wrap style3 ptb-100 bg-rock">
-              <div class="container">
-                <img
-                  src="website-assets-22/images/service-shape-1.webp"
-                  alt="Image"
-                  class="service-shape-one"
-                />
-                <img
-                  src="website-assets-22/images/service-shape-2.webp"
-                  alt="Image"
-                  class="service-shape-two"
-                />
-                <div class="section-title style1 text-center mb-40">
-                  <h2 class="text-white">One API For All Payouts</h2>
+            <section className="service-wrap style3 ptb-100 bg-rock">
+              <div className="container">
+                <div className="section-title style1">
+                  <h2 className="">One API For All Payouts</h2>
                 </div>
-                <div class="row gx-5 align-items-center">
-                  <div class="col-md-4 col-sm-6">
-                    <div class="serviceBox-payout">
-                      <div class="service-icon">
-                        <span>
-                          <i class="fa fa-globe"></i>
-                        </span>
-                      </div>
-                      <h3 class="title">Total Freedom</h3>
-                      <p class="description">
-                        Pay directly from your Pool/Nodal/Current Account,
-                        without blocking funds in your Payment Aggregator’s
-                        account/3rd party wallet
-                      </p>
+                <div className="section-title-grid">
+                  <div className="serviceBox-payout">
+                    <div className="service-icon">
+                      <span>
+                        <Earth style={{ width: "28px", height: "28px" }} />
+                      </span>
                     </div>
+                    <h3 className="title">Total Freedom</h3>
+                    <p className="description">
+                      Pay directly from your Pool/Nodal/Current Account, without
+                      blocking funds in your Payment Aggregators account/3rd
+                      party wallet.
+                    </p>
                   </div>
-                  <div class="col-md-4 col-sm-6">
-                    <div class="serviceBox-payout">
-                      <div class="service-icon">
-                        <span>
-                          <i class="fa fa-globe"></i>
-                        </span>
-                      </div>
-                      <h3 class="title">Inclusive APIs</h3>
-                      <p class="description">
-                        3 Inclusive APIs for Adding Beneficiary, Payout
-                        processing and transaction status.
-                      </p>
+                  <div className="serviceBox-payout">
+                    <div className="service-icon">
+                      <span>
+                        <Earth style={{ width: "28px", height: "28px" }} />
+                      </span>
                     </div>
+                    <h3 className="title">Inclusive APIs</h3>
+                    <p className="description">
+                      3 Inclusive APIs for Adding Beneficiary, Payout
+                      processing, and transaction status.
+                    </p>
                   </div>
-                  <div class="col-md-4 col-sm-6">
-                    <div class="serviceBox-payout">
-                      <div class="service-icon">
-                        <span>
-                          <i class="fa fa-globe"></i>
-                        </span>
-                      </div>
-                      <h3 class="title">Easy Payout Scheduling</h3>
-                      <p class="description">
-                        Easily call the Payout API as and when or to schedule
-                        payouts.
-                      </p>
+                  <div className="serviceBox-payout">
+                    <div className="service-icon">
+                      <span>
+                        <Earth style={{ width: "28px", height: "28px" }} />
+                      </span>
                     </div>
+                    <h3 className="title">Easy Payout Scheduling</h3>
+                    <p className="description">
+                      Easily call the Payout API as and when or to schedule
+                      payouts.
+                    </p>
                   </div>
-                  <div class="col-md-4 col-sm-6">
-                    <div class="serviceBox-payout">
-                      <div class="service-icon">
-                        <span>
-                          <i class="fa fa-globe"></i>
-                        </span>
-                      </div>
-                      <h3 class="title">IP Whitelisting</h3>
-                      <p class="description">
-                        Whitelisted IP for additional security during payouts.
-                      </p>
+
+                  <div className="serviceBox-payout">
+                    <div className="service-icon">
+                      <span>
+                        <Earth style={{ width: "28px", height: "28px" }} />
+                      </span>
                     </div>
+                    <h3 className="title">IP Whitelisting</h3>
+                    <p className="description">
+                      Whitelisted IP for additional security during payouts.
+                    </p>
                   </div>
-                  <div class="col-md-4 col-sm-6">
-                    <div class="serviceBox-payout">
-                      <div class="service-icon">
-                        <span>
-                          <i class="fa fa-globe"></i>
-                        </span>
-                      </div>
-                      <h3 class="title">Secure Authentication</h3>
-                      <p class="description">
-                        A token-based API access system for enhanced security.
-                      </p>
+
+                  <div className="serviceBox-payout">
+                    <div className="service-icon">
+                      <span>
+                        <Earth style={{ width: "28px", height: "28px" }} />
+                      </span>
                     </div>
+                    <h3 className="title">Secure Authentication</h3>
+                    <p className="description">
+                      A token-based API access system for enhanced security.
+                    </p>
                   </div>
-                  <div class="col-md-4 col-sm-6">
-                    <div class="serviceBox-payout">
-                      <div class="service-icon">
-                        <span>
-                          <i class="fa fa-globe"></i>
-                        </span>
-                      </div>
-                      <h3 class="title">Real-time Updates</h3>
-                      <p class="description">
-                        Get real-time updates of all the Transactions.
-                      </p>
+
+                  <div className="serviceBox-payout">
+                    <div className="service-icon">
+                      <span>
+                        <Earth style={{ width: "28px", height: "28px" }} />
+                      </span>
                     </div>
+                    <h3 className="title">Real-time Updates</h3>
+                    <p className="description">
+                      Get real-time updates of all the Transactions.
+                    </p>
                   </div>
                 </div>
               </div>
